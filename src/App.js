b66531c8028f033fb3517efbc2e621be55013a90
Nilesh1908 <nileshap661@gmail.com>
2025-11-20 19:04:25 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
+const customDictionary = {
+  teh: "the",
+  wrok: "work",
+  fot: "for",
+  exampl: "example"
+};
+
+
 function App() {
+
+  const [text, setText] = useState("");
+  const [suggestion, setSuggestion] = useState("");
+
+  const handleChange = (e) => {
+    const inputText = e.target.value
+    setText(inputText)
+    const words = inputText.split("/\s+/");
+
+    const wrongword = words.find((word) => {
+      if(!word) return false;
+      const cleanedword = word.replace(/[.,!?;:]/g, "");
+      return customDictionary.hasOwnProperty(cleanedword.toLowerCase());
+    });
+
+    if(wrongword){
+      const cleanedword = wrongword.replace(/[.,!?;:]/g, "");
+      const correction = customDictionary[cleanedword.toLowerCase()];
+      setSuggestion(`Did you mean ${correction}?`);
+    }else {
+      setSuggestion("");
+    }
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ padding: "20px" }}>
+      <h2>XSpellCheck - React Spell Checker</h2>
+      <textarea
+        rows="5"
+        cols="60"
+        placeholder="Type something..."
+        value={text}
+        onChange={handleChange}
+        style={{ fontSize: "16px", padding: "10px" }}
+      />
+      <div style={{ marginTop: "10px", color: "black", fontWeight: "bold" }}>
+        {suggestion}
+      </div>
     </div>
   );
 }
